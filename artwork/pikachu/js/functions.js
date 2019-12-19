c0e1abca091e9aa873bf0ba1d994dc3479735cff
AGames2911 <agames2911@gmail.com>
2019-12-20 00:14:09 +0100

    master

---
+++ artwork/pikachu/js/functions.js
@@ -0,0 +1,413 @@
+function onload()
+{
+
+}
+
+function pikachu()
+{
+    repeat('grid-block white', 29);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 27);
+
+    repeat('grid-block black', 3);
+    repeat('grid-block white', 26);
+
+    repeat('grid-block black', 3);
+    repeat('grid-block white', 26);
+
+    repeat('grid-block black', 4);
+    repeat('grid-block white', 25);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 25);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 24);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 24);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 19);
+    repeat('grid-block black', 4);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 16);
+    repeat('grid-block black', 7);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 14);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 11);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 2);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 8);
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 3);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 8);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 5);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 16);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 6);
+
+    repeat('grid-block white', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 16);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 8);
+
+    repeat('grid-block white', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 16);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 8);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 8);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 7);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 7);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 4);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 4);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 4);
+    repeat('grid-block black', 3);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block red', 2);
+    repeat('grid-block yellow', 14);
+    repeat('grid-block red', 2);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block red', 4);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block red', 4);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block red', 4);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block red', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block red', 2);
+    repeat('grid-block yellow', 14);
+    repeat('grid-block red', 2);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 20);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 18);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+
+    repeat('grid-block white', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 16);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 17);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 18);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 3);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 19);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 20);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 7);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 1);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 3);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 4);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 2);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 5);
+
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 7);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 4);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 6);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 6);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 19);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 7);
+
+    repeat('grid-block white', 1);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 19);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 7);
+
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 17);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 8);
+
+    repeat('grid-block white', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 14);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 9);
+
+    repeat('grid-block white', 5);
+    repeat('grid-block black', 2);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 4);
+    repeat('grid-block yellow', 3);
+    repeat('grid-block black', 2);
+    repeat('grid-block white', 10);
+
+    repeat('grid-block white', 4);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 2);
+    repeat('grid-block black', 1);
+    repeat('grid-block yellow', 5);
+    repeat('grid-block black', 1);
+    repeat('grid-block white', 9);
+
+    repeat('grid-block white', 4);
+    repeat('grid-block black', 6);
+    repeat('grid-block white', 4);
+    repeat('grid-block black', 6);
+    repeat('grid-block white', 9);
+
+    repeat('grid-block white', 29);
+}
+
+function repeat(string, amount)
+{
+    var i;
+    for (i = 0; i < amount; i++)
+    {
+        var element = document.createElement("div");
+        element.className = string;
+        element.appendChild(document.createTextNode(""));
+        document.getElementById("container").appendChild(element)
+    }
+}
+
+function test()
+{
+    document.getElementById("demo").innerHTML = "test";
+}
---
+++ js/functions.js
@@ -0,0 +1,12 @@
+function init() {
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("nav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+}
